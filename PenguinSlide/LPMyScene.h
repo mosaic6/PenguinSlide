7fe071e7c93299ff6acb6b2dd25c1539d52ca3cf
Joshua Walsh <joshuatwalsh@gmail.com>
2014-06-03 21:18:05 -0400

    Adding initial project files

---
+++ PenguinSlide/LPMyScene.h
@@ -0,0 +1,13 @@
+//
+//  LPMyScene.h
+//  PenguinSlide
+//
+
+//  Copyright (c) 2014 Joshua Walsh. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface LPMyScene : SKScene
+
+@end
---
+++ PenguinSlide/LPViewController.h
@@ -0,0 +1,14 @@
+//
+//  LPViewController.h
+//  PenguinSlide
+//
+
+//  Copyright (c) 2014 Joshua Walsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface LPViewController : UIViewController
+
+@end
