c203bbe746fc71c32d666283da3b52a6659bf83e
Joshua Walsh <joshuatwalsh@gmail.com>
2014-06-03 21:23:37 -0400

    eh

---
+++ PenguinSlide/PenguinSlide/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PenguinSlide
+//
+//  Created by Joshua T Walsh on 6/3/14.
+//  Copyright Joshua Walsh 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
